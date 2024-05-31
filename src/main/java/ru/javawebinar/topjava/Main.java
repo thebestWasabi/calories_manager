838278cd5d90175a326f3fae4cddb64f7f84e912
wasabi <openpw@icloud.com>
2024-05-31 06:12:57 +0300

    add: initial project data
    Добавил начальные данные

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -5,7 +5,9 @@ package ru.javawebinar.topjava;
  * @see <a href="https://github.com/JavaOPs/topjava">Initial project</a>
  */
 public class Main {
+
     public static void main(String[] args) {
         System.out.format("Hello TopJava Enterprise!");
     }
+
 }
