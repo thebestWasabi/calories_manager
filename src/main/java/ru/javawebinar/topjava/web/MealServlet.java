d010e5007b29cba85a4787d38a2e6733f39d4c68
wasabi <openpw@icloud.com>
2024-06-09 22:54:42 +0300

    refactor: meals.jsp & MealServlet
    Обновил классы

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,7 +1,8 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
-import ru.javawebinar.topjava.constant.MealsConst;
+import ru.javawebinar.topjava.db.ICrud;
+import ru.javawebinar.topjava.db.InMemoryDb;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.MealTo;
 import ru.javawebinar.topjava.util.MealsUtil;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -18,15 +20,29 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class MealServlet extends HttpServlet {
 
     private static final Logger log = getLogger(MealServlet.class);
+    private final ICrud db = new InMemoryDb();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        final List<Meal> meals = MealsConst.meals;
+        final List<Meal> meals = db.getAll();
         final List<MealTo> mealsDto = MealsUtil.toDto(meals);
 
         log.debug("redirect to meals");
 
         req.setAttribute("meals", mealsDto);
         req.getRequestDispatcher("/meals.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        final LocalDateTime date = LocalDateTime.parse(req.getParameter("dateTime"));
+        final String description = req.getParameter("description");
+        final int calories = Integer.parseInt(req.getParameter("calories"));
+
+        final Meal meal = new Meal(date, description, calories);
+        db.add(meal);
+
+        resp.sendRedirect(req.getContextPath() + "/meals");
     }
 }
