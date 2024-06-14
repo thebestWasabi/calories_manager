da3fc62ec171140c000e930487122d4b248c3471
wasabi <openpw@icloud.com>
2024-06-14 06:04:52 +0300

    refactor

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -15,10 +15,10 @@ public class UserServlet extends HttpServlet {
     private static final Logger log = getLogger(UserServlet.class);
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("redirect to users");
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
 
+        log.debug("redirect to users");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
-//        response.sendRedirect("users.jsp");
     }
 }
