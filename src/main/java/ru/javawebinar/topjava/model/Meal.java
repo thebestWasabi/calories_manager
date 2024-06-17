9c9ca5484bd72a3de5862c6c6b5e57ded661b8c2
wasabi <openpw@icloud.com>
2024-06-17 21:29:39 +0300

    refactor: Meal

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -4,23 +4,22 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-public class Meal {
+public class Meal extends AbstractBaseEntity {
 
-    private Integer id;
     private final LocalDateTime dateTime;
     private final String description;
     private final int calories;
 
     public Meal(final Integer id, final LocalDateTime dateTime,
                 final String description, final int calories) {
-        this.id = id;
+        super(id);
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
     }
 
-    public Meal(final LocalDateTime dateTime,
-                final String description, final int calories) {
+    public Meal(final LocalDateTime dateTime, final String description,
+                final int calories) {
         this(null, dateTime, description, calories);
     }
 
@@ -28,18 +27,6 @@ public class Meal {
     public String toString() {
         return String.format("Meal {id=%d, dateTime=%s, description='%s', calories=%d}",
                 id, dateTime, description, calories);
-    }
-
-    public boolean isNew() {
-        return id == null;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(final Integer id) {
-        this.id = id;
     }
 
     public LocalDateTime getDateTime() {
