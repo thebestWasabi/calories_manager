d4e3e5a66ab975c8868f965102944e05503f8d40
wasabi <openpw@icloud.com>
2024-06-17 21:29:03 +0300

    refactor: InMemoryMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
@@ -11,6 +13,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class InMemoryMealRepository implements MealRepository {
 
+    private final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
+
     private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
@@ -19,7 +23,8 @@ public class InMemoryMealRepository implements MealRepository {
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(final Meal meal) {
+        log.info("save {}", meal);
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
             repository.put(meal.getId(), meal);
@@ -30,17 +35,20 @@ public class InMemoryMealRepository implements MealRepository {
     }
 
     @Override
+    public boolean delete(final int id) {
+        log.info("delete {}", id);
+        return repository.remove(id) != null;
+    }
+
+    @Override
     public Meal get(final int id) {
+        log.info("get {}", id);
         return repository.get(id);
     }
 
     @Override
     public Collection<Meal> getAll() {
+        log.info("getAll");
         return repository.values();
-    }
-
-    @Override
-    public boolean delete(final int id) {
-        return repository.remove(id) != null;
     }
 }
